88c0ac3ce6447a72a148ef0abd7bc0d58fab98e2
1polygon <bennija@web.de>
2019-02-14 17:18:54 +0100

    changed to timer for mouse move events to reduce repaints

--- ScreenSnip/Overlay.cs
+++ ScreenSnip/Overlay.cs
@@ -3,20 +3,28 @@ using System.Drawing;
 using System.Windows.Forms;
 
 namespace ScreenSnip {
+    class OverlayForm : Form {
+        public OverlayForm() {
+            SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+        }
+    }
+
     class Overlay {
         private static Form form;
         private static Panel box;
         private static Label info;
-        
+        private static Timer timer;
+
+        private static Point mousePos;
         private static bool selecting = false;
         private static Screen.Selection selection;
 
         public static void Init() {
-            form = new Form();
+            form = new OverlayForm();
 
-            MouseMoveMessageFilter mouseMoveFilter = new MouseMoveMessageFilter();
-            mouseMoveFilter.TargetForm = form;
-            Application.AddMessageFilter(mouseMoveFilter);
+            MouseMessageFilter mouseFilter = new MouseMessageFilter();
+            mouseFilter.TargetForm = form;
+            Application.AddMessageFilter(mouseFilter);
 
             form.FormBorderStyle = FormBorderStyle.None;
             form.TransparencyKey = Color.Turquoise;
@@ -31,15 +39,37 @@ namespace ScreenSnip {
             box.BorderStyle = BorderStyle.FixedSingle;
 
             info = new Label();
-
             box.Controls.Add(info);
             form.Controls.Add(box);
 
-            form.MouseClick += overlayMouseClick;
+            timer = new Timer();
+            timer.Interval = 12;
+            timer.Tick += timerTick;
+            timer.Start();
+
             form.KeyDown += overlayKeyDown;
             info.Paint += infoDraw;
-
             Application.Run(form);
+        }
+
+        private static void timerTick(object sender, EventArgs e) {
+            if(mousePos.X != Control.MousePosition.X || mousePos.Y != Control.MousePosition.Y) {
+                mousePos.X = Control.MousePosition.X;
+                mousePos.Y = Control.MousePosition.Y;
+                if (selecting) {
+                    selection.p2 = new Point(Control.MousePosition.X, Control.MousePosition.Y);
+                    selection.Update();
+                    box.Visible = false;
+                    box.Left = Screen.left * -1 + selection.x;
+                    box.Top = Screen.top * -1 + selection.y;
+                    box.Width = selection.w + 1;
+                    box.Height = selection.h + 1;
+                    box.Visible = true;
+                    if (selection.p2.X < selection.p1.X) info.Top = selection.h - 17;
+                    else info.Top = 1;
+                }
+                info.Invalidate();
+            }
         }
 
         private static void infoDraw(object sender, PaintEventArgs e) {
@@ -47,41 +77,30 @@ namespace ScreenSnip {
             e.Graphics.DrawString(selection.w + " x " + selection.h, info.Font, SystemBrushes.GrayText, new Point(0, 0));
         }
 
-        class MouseMoveMessageFilter : IMessageFilter {
+        class MouseMessageFilter : IMessageFilter {
             public Form TargetForm { get; set; }
+
             public bool PreFilterMessage(ref Message m) {
-                if (m.Msg == 512 || m.Msg == 160) {
-                    if (selecting) {
+                if (m.Msg == 513 || m.Msg == 162) { // left click
+                    if (!selecting) {
+                        selecting = true;
+                        box.Visible = true;
+                        selection.p1 = new Point(Control.MousePosition.X, Control.MousePosition.Y);
+                    } else {
                         selection.p2 = new Point(Control.MousePosition.X, Control.MousePosition.Y);
                         selection.Update();
-                        box.Left = Screen.left * -1 + selection.x;
-                        box.Top = Screen.top * -1 + selection.y;
-                        box.Size = new Size(selection.w, selection.h);
+                        form.Visible = false;
+                        ScreenSnip.Snip(selection.x, selection.y, selection.w, selection.h);
+                        Application.Exit();
                     }
-                    info.Invalidate();
-                }
+                } else if (m.Msg == 517 || m.Msg == 165) Application.Exit(); // right click
+
                 return false;
             }
         }
 
         private static void overlayKeyDown(object sender, KeyEventArgs e) {
             if (e.KeyCode == Keys.Escape) Application.Exit();
-        }
-
-        private static void overlayMouseClick(object sender, MouseEventArgs e) {
-            if(e.Button == MouseButtons.Left) {
-                if (!selecting) {
-                    selecting = true;
-                    box.Visible = true;
-                    selection.p1 = new Point(Control.MousePosition.X, Control.MousePosition.Y);
-                } else {
-                    selection.p2 = new Point(Control.MousePosition.X, Control.MousePosition.Y);
-                    selection.Update();
-                    box.Visible = false;
-                    ScreenSnip.Snip(selection.x, selection.y, selection.w, selection.h);
-                    Application.Exit();
-                }
-            } else Application.Exit();
         }
     }
 }
